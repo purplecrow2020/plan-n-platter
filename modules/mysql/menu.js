90994d3aa505550546c27ca622048b56d0bf7581
Akshat <akshat@sysadmins-MacBook-Pro.local>
2022-06-11 02:13:21 +0530

    fix the is_active check

--- modules/mysql/menu.js
+++ modules/mysql/menu.js
@@ -9,7 +9,7 @@ module.exports = class Menu {
     }
 
     static getMenuByVendorIdWithOrderDetails(database, vendor_id, order_id) {
-        const sql = "select * from ((select * from menus where vendor_id = ?) as a left join (select  menu_id, count(id) as qty from cart where order_id=? and is_ordered !=1 group by menu_id) as b on a.id=b.menu_id)";
+        const sql = "select * from ((select * from menus where vendor_id = ? and is_active=1) as a left join (select  menu_id, count(id) as qty from cart where order_id=? and is_ordered !=1 group by menu_id) as b on a.id=b.menu_id)";
         return database.query(sql, [vendor_id,order_id]);
     }
     
